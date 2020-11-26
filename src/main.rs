412d50bea70e4b5fb77aedf30dbf7e230266d847
Andrew O <andrew.cc.osborne@gmail.com>
2020-11-26 15:54:23 -0800

    adjusting camera, moving earth to better spot, first crack at end condition

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,22 @@ use bevy::prelude::*;
 fn main() {
     App::build()
         .add_resource(Msaa { samples: 4 })
+        .add_resource(GameState::Running)
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
         .add_system(player_control_update.system())
+        .add_system(test_end_condition.system())
         .run();
 }
 
+#[allow(dead_code)]
+#[derive(PartialEq)]
+enum GameState {
+    Running,
+    Paused,
+    GameOver,
+    Won
+}
 
 fn player_control_update(
     time: Res<Time>,
@@ -35,6 +45,7 @@ fn player_control_update(
         *transform.translation.z_mut() = new_z;
     }
 }
+
 #[derive(Debug, Default, PartialEq, Clone, Copy, Properties)]
 struct Velocity{
     pub velocity: Vec3,
@@ -42,16 +53,21 @@ struct Velocity{
 
 struct PlayerControl;
 
-fn add_tank(
+struct EarthMarker;
+
+
+
+fn add_ship(
     commands: &mut Commands,
     meshes: &mut ResMut<Assets<Mesh>>,
     materials: &mut ResMut<Assets<StandardMaterial>>,
+    position: Vec3,
 ) {
     commands
         .spawn(PbrComponents {
             mesh: meshes.add(Mesh::from(shape::Cube { size: 2.0 })),
             material: materials.add(Color::rgb(1., 0.9, 0.9).into()),
-            transform: Transform::from_translation(Vec3::new(4., 0., 4.)),
+            transform: Transform::from_translation(position),
             ..Default::default()
         })
         .with(Velocity::default())
@@ -61,15 +77,17 @@ fn add_tank(
 fn add_earth(
     commands: &mut Commands,
     meshes: &mut ResMut<Assets<Mesh>>,
-    materials: &mut ResMut<Assets<StandardMaterial>>
+    materials: &mut ResMut<Assets<StandardMaterial>>,
+    position: Vec3
 ) {
     commands
         .spawn(PbrComponents {
             mesh: meshes.add(Mesh::from(shape::Icosphere { radius: 2.0, ..Default::default() })),
             material: materials.add(Color::rgb(1., 0.9, 0.9).into()),
-            transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
+            transform: Transform::from_translation(position),
             ..Default::default()
-        });
+        })
+        .with(EarthMarker);
 }
 
 fn setup(
@@ -77,12 +95,13 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+
     commands
         // Camera
         .spawn(Camera3dComponents {
             transform: Transform::from_matrix(Mat4::from_rotation_translation(
-                Quat::from_xyzw(-0.3, -0.5, -0.3, 0.5).normalize(),
-                Vec3::new(-7.0, 20.0, 4.0),
+                Quat::from_xyzw(-0.3, -1.0, -0.3, 1.0).normalize(),
+                Vec3::new(-7.0, 20.0, 0.0),
             )),
             ..Default::default()
         })
@@ -92,6 +111,39 @@ fn setup(
             ..Default::default()
         });
 
-    add_tank(&mut commands, &mut meshes, &mut materials);
-    add_earth(&mut commands, &mut meshes, &mut materials);
+    add_ship(&mut commands, &mut meshes, &mut materials, Vec3::new(4.0, 0., 0.0));
+    add_earth(&mut commands, &mut meshes, &mut materials, Vec3::new(40.0, 0.0, 0.0));
+    
+}
+
+fn calc_dist_sq(pos1: Vec3,
+    pos2: Vec3
+) -> f32 {
+    let diff = pos1 - pos2;
+    diff.dot(diff)
+}
+
+fn test_end_condition( mut game_state: ResMut<GameState>,
+    player_query: Query<(&PlayerControl, &Transform)>,
+    earth_query: Query<(&EarthMarker, &Transform)>, 
+) {
+
+    if *game_state != GameState::Running {
+        return;
+    }
+
+    const VICTORY_DISTANCE: f32 = 10.0;
+    let distance_sq:f32 = VICTORY_DISTANCE * VICTORY_DISTANCE;
+
+    for (_, player_transform) in player_query.iter() {
+       
+        for (_, earth_transform) in earth_query.iter() {
+
+            if calc_dist_sq(player_transform.translation, earth_transform.translation) <= distance_sq {
+                // insert end of game message here!!
+                println!("YOU WIN!!");
+                *game_state = GameState::Won;
+            }
+        }
+    }
 }
