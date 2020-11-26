08de55579d852d61e782d7e56defc1b76f862740
ian <ianmccall93@gmail.com>
2020-11-26 15:34:42 -0800

    Make the camera the child of the player object
    So that it follows the player around

--- src/main.rs
+++ src/main.rs
@@ -44,9 +44,6 @@ fn velocity_update(
     mut query: Query<(&mut Transform, &mut  Velocity)>,
 ) {
     for (mut transform, velocity) in query.iter_mut() {
-
-        dbg!(velocity.velocity);
-
         let displacement = velocity.velocity * time.delta_seconds;
         transform.translation += displacement;
     }
@@ -67,7 +64,18 @@ fn add_tank(
             ..Default::default()
         })
         .with(Velocity::default())
-        .with(PlayerControl);
+        .with(PlayerControl)
+        .with_children(|parent| {
+
+            // Camera
+            parent.spawn(Camera3dComponents {
+                transform: Transform::from_matrix(Mat4::from_rotation_translation(
+                    Quat::from_xyzw(-0.3, -0.5, -0.3, 0.5).normalize(),
+                    Vec3::new(-7.0, 20.0, 4.0),
+                )),
+                ..Default::default()
+            });
+        });
 }
 
 fn add_earth(
@@ -90,14 +98,6 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     commands
-        // Camera
-        .spawn(Camera3dComponents {
-            transform: Transform::from_matrix(Mat4::from_rotation_translation(
-                Quat::from_xyzw(-0.3, -0.5, -0.3, 0.5).normalize(),
-                Vec3::new(-7.0, 20.0, 4.0),
-            )),
-            ..Default::default()
-        })
         // Light
         .spawn(LightComponents {
             transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
