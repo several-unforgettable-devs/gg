fbb11c36520d5daf26ab92e08075d5d6076b3cc6
ian <ianmccall93@gmail.com>
2020-11-27 16:10:32 -0800

    Remove some goofy debug prints

--- src/audio.rs
+++ src/audio.rs
@@ -2,9 +2,6 @@
 use bevy::prelude::*;
 
 pub fn setup_audio(asset_server: Res<AssetServer>, audio: Res<Audio>) {
-
-    println!("Hello!");
     let music = asset_server.load("audio/BackgroundMusicLoop.mp3");
     audio.play(music);
-    println!("Goodbye!");
 }
