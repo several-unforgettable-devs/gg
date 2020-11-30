bd2bd8d117e113b9ee682a097a77ab2a6c35ba37
Andrew O <andrew.cc.osborne@gmail.com>
2020-11-29 23:12:26 -0800

    adding pause message

--- src/game_messaging.rs
+++ src/game_messaging.rs
@@ -3,31 +3,63 @@ use bevy::prelude::*;
 
 pub struct GameMessagePlugin;
 
+
+
+struct PauseMessageMarker;
+
 struct GameMessageState {
     displayed: bool,
+    pause_menu_displayed: bool,
 }
 
 impl Plugin for GameMessagePlugin {
     fn build(&self, app: &mut AppBuilder) {
         app.add_startup_system(setup_ui)
-            .add_resource(GameMessageState { displayed: false })
+            .add_resource(GameMessageState { displayed: false, pause_menu_displayed: false })
             .add_system(update_ui);
     }
     // span 1 quad, re-draw very 60ms
 }
 
-fn spawn_message(commands: &mut Commands, asset_server: Res<AssetServer>, message: String) {
+fn spawn_title_message(commands: &mut Commands, 
+    asset_server: &Res<AssetServer>, 
+    message: String,
+) {
+    let position = Rect {
+        top: Val::Px(5.0),
+        left: Val::Px(5.0),
+        right: Val::Px(5.0),
+        ..Default::default()
+    };
+    spawn_message(commands, asset_server, message, position, false);
+}
+
+fn spawn_pause_message(commands: &mut Commands, 
+    asset_server: &Res<AssetServer>, 
+) {
+    let position = Rect {
+        top: Val::Percent(40.0),
+        ..Default::default()
+    };
+    spawn_message(commands, 
+        asset_server,
+        String::from("click the screen to unpause"), 
+        position,
+        true);
+}
+
+fn spawn_message(commands: &mut Commands, 
+    asset_server: &Res<AssetServer>, 
+    message: String,
+    position: Rect<Val>,
+    is_pause_message:bool,
+) {
     let font = asset_server.load("fonts/FiraSans-Bold.ttf");
-    commands.spawn(TextBundle {
+    let spawner = commands.spawn(TextBundle {
         style: Style {
             size: Size::new(Val::Percent(100.0), Val::Px(100.0)),
             position_type: PositionType::Absolute,
-            position: Rect {
-                top: Val::Px(5.0),
-                left: Val::Px(5.0),
-                right: Val::Px(5.0),
-                ..Default::default()
-            },
+            position: position,
             ..Default::default()
         },
         text: Text {
@@ -44,9 +76,15 @@ fn spawn_message(commands: &mut Commands, asset_server: Res<AssetServer>, messag
         },
         ..Default::default()
     });
+
+    if is_pause_message {
+        spawner.with(PauseMessageMarker);
+    }
 }
 
-fn setup_ui(commands: &mut Commands) {
+fn setup_ui(
+    commands: &mut Commands
+) {
     commands.spawn(UiCameraBundle::default());
 }
 
@@ -55,22 +93,40 @@ fn update_ui(
     game_state: Res<GameState>,
     commands: &mut Commands,
     asset_server: Res<AssetServer>,
+    mut pause_msg_query: Query<(&PauseMessageMarker, Entity)>
 ) {
-    if message_state.displayed {
-        return;
+
+    if !message_state.displayed {
+        if *game_state == GameState::Lost {
+            // spawn "Game Over"
+            spawn_title_message(
+                commands,
+                &asset_server,
+                String::from("You have brought shame to your family..."),
+            );
+            message_state.displayed = true;
+        } else if *game_state == GameState::Won {
+            // spawn "You Win"
+            spawn_title_message(commands, &asset_server, String::from("ANTICS ACHIEVED!!"));
+            message_state.displayed = true;
+        }
+    }
+    
+
+
+    let add_pause_menu:bool = !message_state.pause_menu_displayed && *game_state == GameState::Paused;
+    let remove_pause_menu:bool = message_state.pause_menu_displayed && *game_state != GameState::Paused;
+
+    if add_pause_menu {
+        spawn_pause_message(commands, &asset_server);
+        message_state.pause_menu_displayed = true;
     }
 
-    if *game_state == GameState::Lost {
-        // spawn "Game Over"
-        spawn_message(
-            commands,
-            asset_server,
-            String::from("You have brought shame to your family..."),
-        );
-        message_state.displayed = true;
-    } else if *game_state == GameState::Won {
-        // spawn "You Win"
-        spawn_message(commands, asset_server, String::from("ANTICS ACHIEVED!!"));
-        message_state.displayed = true;
+    if remove_pause_menu {
+        for (_, entity) in pause_msg_query.iter_mut() {
+            commands.despawn(entity);
+            message_state.pause_menu_displayed = false;
+        }
     }
+
 }
