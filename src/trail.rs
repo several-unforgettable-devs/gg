1bd28d7f03ba0ac0b0a645c667b50af64ef31797
Andrew O <andrew.cc.osborne@gmail.com>
2020-11-29 16:27:49 -0800

    a beautiful trail :)

--- src/trail.rs
+++ src/trail.rs
@@ -1,35 +1,30 @@
 use bevy::prelude::*;
+use rand::Rng;
 use crate::input::PlayerInput;
 use crate::velocity::Velocity;
 
 pub struct MotionTrailPlugin;
 
+// Settings
 const RESPAWN_RATE:f32 = 0.2;
 const TRAIL_SIZE:f32 = 0.05;
 const MAX_TRAIL_DIST:f32 = 10.0;
+const SPAWN_ANGLE_RADIUS:f32 = std::f32::consts::PI / 2.0;
+const NUM_OF_TRAIL_PARTICLES:i32 = 40;
 
-struct TrailManagement {
-    reset_timer: Timer 
-}
+// Internal derived constants
+const MAX_TRAIL_DIST_SQ:f32 = MAX_TRAIL_DIST * MAX_TRAIL_DIST;
+const SPAWN_DISTANCE:f32 = MAX_TRAIL_DIST * 0.75;
 
-impl TrailManagement {
-    fn new() -> TrailManagement {
-        TrailManagement{ reset_timer: Timer::from_seconds(RESPAWN_RATE,true)}
-    }
-}
 
-struct TrailObj
-{
-    radial_offset:f32,
-    angle:f32, // in radians, 0-degress is 'up' on ship.
-}
+
+struct TrailObj;
 
 impl Plugin for MotionTrailPlugin
 {
     fn build(&self, app: &mut AppBuilder)
     {
         app.add_startup_system(setup_trail)
-            .add_resource(TrailManagement::new())
             .add_system(update_trail);
     }
     // span 1 quad, re-draw very 60ms
@@ -37,8 +32,7 @@ impl Plugin for MotionTrailPlugin
 
 fn make_trail_obj(commands: &mut Commands,
     trail_material: &Handle<StandardMaterial>,
-    trail_mesh: &Handle<Mesh>,
-    angle_in_degrees: f32
+    trail_mesh: &Handle<Mesh>
 ) {
     commands.spawn(PbrBundle {
         mesh: trail_mesh.clone(),
@@ -48,7 +42,7 @@ fn make_trail_obj(commands: &mut Commands,
         },
         ..Default::default()
     })
-        .with(TrailObj{ radial_offset: 2.5, angle: f32::to_radians(angle_in_degrees) });
+        .with(TrailObj);
 }
 
 fn setup_trail(commands: &mut Commands,
@@ -56,54 +50,77 @@ fn setup_trail(commands: &mut Commands,
     mut materials: ResMut<Assets<StandardMaterial>>
 ) {
     let trail_material = materials.add(Color::rgb(0.75, 0.75, 1.0).into());
-    /*let trail_mesh = meshes.add(Mesh::from(shape::Quad {
-        size: Vec2::new(0.2, 0.2),
-        flip: false
-    }));*/
     let trail_mesh = meshes.add(Mesh::from(shape::Cube {
         size: TRAIL_SIZE
     }));
-    make_trail_obj(commands, &trail_material, &trail_mesh, 45.0);
-    make_trail_obj(commands, &trail_material, &trail_mesh, -45.0);
+
+    for _ in 1..NUM_OF_TRAIL_PARTICLES {
+        make_trail_obj(commands, &trail_material, &trail_mesh);
+    }
+    
+}
+
+fn build_quat_to_align_vec(
+    original_vector : Vec3,
+    new_direction : Vec3
+) -> Quat {
+    let quat_vector = original_vector.cross(new_direction).normalize();
+    let quat_angle = new_direction.x.acos();
+    Quat::from_axis_angle(quat_vector, quat_angle)
 }
 
 fn build_trail_quat(velocity: &Vec3) -> Quat {
     let velocity_copy = velocity.clone().normalize();
-
-    let quat_vector = Vec3::unit_x().cross(velocity_copy).normalize();
-    let quat_angle = velocity_copy.x.acos();
-    return Quat::from_axis_angle(quat_vector, quat_angle);
+    build_quat_to_align_vec(Vec3::unit_x(), velocity_copy)
 }
 
 
-const MAX_TRAIL_DIST_SQ:f32 = MAX_TRAIL_DIST * MAX_TRAIL_DIST;
+fn valid_vec3(vec: &Vec3) -> bool {
+    !vec.is_nan().any()
+}
+
+#[allow(dead_code)]
+fn calc_trail_spawn_point(
+    player_position : Vec3,
+    player_up : Vec3,
+    player_velocity_unit : Vec3,
+) -> Vec3 {
+    let mut rng = rand::thread_rng();
+    let projected_radius = SPAWN_DISTANCE * (SPAWN_ANGLE_RADIUS / 2.0).sin();
+    let projected_dir = if valid_vec3(&player_velocity_unit) { player_velocity_unit } else { Vec3::zero() };
+    let x_mag = rng.gen_range(-1.0,1.0);
+    let y_mag = rng.gen_range(-1.0,1.0);
+    let x_dir = player_up.cross(projected_dir).normalize();
+    let y_dir = x_dir.cross(projected_dir).normalize();
+
+    let final_pos = player_position + projected_dir * MAX_TRAIL_DIST * 0.75
+        + x_mag * x_dir * projected_radius
+        + y_mag * y_dir * projected_radius;
+    final_pos
+}
 
 fn update_trail(
-    time : Res<Time>,
-    mut trail_mgr : ResMut<TrailManagement>,
     mut trail_query : Query<(&TrailObj, &mut Transform)>,
     player_query : Query<(&PlayerInput, &Transform, &Velocity)>
 ) {
-    trail_mgr.reset_timer.tick(time.delta_seconds);
     
-    //let respwan = trail_mgr.reset_timer.just_finished();
-
     for (_, player_transform, velocity_info) in player_query.iter() {
         let player_up = player_transform.rotation * Vec3::unit_y();
-        let player_forward = player_transform.forward();
         let player_velocity = velocity_info.velocity.length();
-        //let player_velocity_dir = velocity_info.velocity.clone().normalize();
+        let player_velocity_dir = velocity_info.velocity.clone().normalize();
+
+        if !valid_vec3(&player_velocity_dir) {
+            return;
+        }
+
         let trail_quat = build_trail_quat(&velocity_info.velocity);
-        for (trail_info, mut transform) in trail_query.iter_mut() {
+        for (_, mut transform) in trail_query.iter_mut() {
 
             let vector_from_player = transform.translation - player_transform.translation;
-            //let respawn:bool = ;
 
             let new_translation = 
                 if vector_from_player.length_squared() > MAX_TRAIL_DIST_SQ {
-                    let rotation_quat = Quat::from_axis_angle(player_forward, trail_info.angle);
-                    let offset_direction = rotation_quat.mul_vec3(player_up);
-                    player_transform.translation + offset_direction * trail_info.radial_offset
+                    calc_trail_spawn_point( player_transform.translation, player_up, player_velocity_dir)
                 } else {
                     transform.translation
                 };
@@ -113,9 +130,7 @@ fn update_trail(
                 rotation: trail_quat,
                 scale: Vec3::new(player_velocity, 1.0, 1.0),
             };
-            
-
-            //*transform = transform_copy;
+        
         }
     }
     
